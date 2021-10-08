1d155b7e6be4dc3a41fc1f881fd1d21ab5569474
JacekNiedzielski <niedzielskijacek@gmail.com>
2021-10-08 16:19:13 +0200

    Changes made on 08/10/2021

---
+++ beam_insertion_intoDataBase.py
@@ -0,0 +1,25 @@
+import sys, os
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtCore import *
+import math
+from PIL import Image
+import sqlite3
+
+
+
+class Insertion(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Beam Entry")
+        self.setWindowIcon(QIcon("icons/ico.ico"))
+        self.setGeometry(450,150,500,550)
+        self.setStyleSheet("background-color:white")
+        self.UI()
+        self.show()
+
+
+    def UI(self):
+        pass
--- vst_beam.py
+++ vst_beam.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import *
 import math
 from PIL import Image
 import sqlite3
-
+import beam_insertion_intoDataBase
 
 
 class AddBeam(QWidget):
@@ -29,6 +29,7 @@ class AddBeam(QWidget):
     def widgets(self):
         global defaultImage
         defaultImage = QPixmap("img/img1.jpg")
+        self.spacingWidget = QLabel("  ")
         self.confirmReinforcementBtn = QPushButton("Confirm number of rebars")
         self.confirmReinforcementBtn.clicked.connect(self.make_widgets)
         self.confirmDiametersBtn = QPushButton("Confirm diameters")
@@ -52,16 +53,21 @@ class AddBeam(QWidget):
         for number in range(2,11,1):
             self.bottom_reinf_number.addItem(str(number))
 
+        self.saveBtn = QPushButton("Save into Database and Show Info")
+        self.saveBtn.clicked.connect(self.save_and_getInfo)
 
 
     def layouts(self):
-        self.topMainLayout = QHBoxLayout()
-        self.topLeftLayout = QVBoxLayout()
+        self.mainLayout = QVBoxLayout()
 
+        self.bottomMainLayout = QHBoxLayout()
+        self.topMainLayout = QHBoxLayout()
+
+        self.bottomMainLayout.addWidget(self.saveBtn)
+
+        self.topLeftLayout = QVBoxLayout()
         self.topLeftLayout.addWidget(self.Cross_Section_IMG)
         self.topLeftLayout.setAlignment(Qt.AlignCenter)
-
-
         self.topRightLayout = QGridLayout()
         self.topveryRightLayout = QVBoxLayout()
 
@@ -77,20 +83,11 @@ class AddBeam(QWidget):
         confirm_Diameter_Label = QLabel(self.tr("&Confirm chosen diameters"))
         confirm_Diameter_Label.setBuddy(self.confirmDiametersBtn)
 
-        choose_PositionsTop_Label  = QLabel(self.tr("""&Choose positions of rebars
-staring from the very 
-top left and ending with
-the very bottom right"""))
-        choose_PositionsTop_Label.setBuddy(self.choosePositionsTopBtn)
+        self.choose_PositionsTop_Label  = QLabel(self.tr(" "))
+        self.choose_PositionsTop_Label.setBuddy(self.choosePositionsTopBtn)
 
-        choose_PositionsBottom_Label = QLabel(self.tr("""&Choose positions of rebars
-staring from the very 
-top left and ending with
-the very bottom right"""))
-        choose_PositionsBottom_Label.setBuddy(self.choosePositionsBottomBtn)
-
-
-
+        self.choose_PositionsBottom_Label = QLabel(self.tr(" "))
+        self.choose_PositionsBottom_Label.setBuddy(self.choosePositionsBottomBtn)
 
         self.topRightLayout.addWidget(top_reinf_number_Label, 0,0)
         self.topRightLayout.addWidget(self.top_reinf_number, 0,1)
@@ -98,11 +95,13 @@ the very bottom right"""))
         self.topRightLayout.addWidget(self.bottom_reinf_number, 1, 1)
         self.topRightLayout.addWidget(self.confirmReinforcementBtn, 2, 0)
         self.topRightLayout.addWidget(self.confirmDiametersBtn, 2, 1)
-        self.topRightLayout.addWidget(choose_PositionsTop_Label, 4, 0)
+        self.topRightLayout.addWidget(self.choose_PositionsTop_Label, 4, 0)
         self.topRightLayout.addWidget(self.choosePositionsTopBtn,4,1)
-        self.topRightLayout.addWidget(choose_PositionsBottom_Label, 5, 0)
-        self.topRightLayout.addWidget(self.choosePositionsBottomBtn,5,1)
-
+        self.topRightLayout.addWidget(self.spacingWidget, 5,0)
+        self.topRightLayout.addWidget(self.spacingWidget, 5,1)
+        self.topRightLayout.addWidget(self.spacingWidget, 5,2)
+        self.topRightLayout.addWidget(self.choose_PositionsBottom_Label, 6, 0)
+        self.topRightLayout.addWidget(self.choosePositionsBottomBtn,6,1)
 
         try:
             self.topRightLayout.addLayout(self.topRebars, 0, 2)
@@ -111,14 +110,17 @@ the very bottom right"""))
         except:
             pass
 
+
         self.topMainLayout.addLayout(self.topLeftLayout)
         self.topMainLayout.addLayout(self.topRightLayout)
         self.topMainLayout.addLayout(self.topveryRightLayout)
         self.topMainLayout.setAlignment(Qt.AlignTop)
 
+        self.mainLayout.addLayout(self.topMainLayout)
+        self.mainLayout.addLayout(self.bottomMainLayout)
 
 
-        self.setLayout(self.topMainLayout)
+        self.setLayout(self.mainLayout)
 
 
     def make_widgets(self):
@@ -229,10 +231,17 @@ the very bottom right"""))
             QMessageBox.information(self, "Info", "Entries cannot be empty")
 
     def define_top_rebars_positions(self):
-        beam_image = QPixmap("img/Coordinaten.jpg")
+        beam_image = QPixmap("img/Coordinates_Top.jpg")
         beam_image = beam_image.scaledToWidth(256)
         beam_image = beam_image.scaledToHeight(256)
         self.Cross_Section_IMG.setPixmap(beam_image)
+
+        self.choose_PositionsTop_Label = QLabel(self.tr("""&Choose positions of rebars
+staring from the very 
+top left and ending with
+the very bottom right"""))
+        self.choose_PositionsTop_Label.setBuddy(self.choosePositionsTopBtn)
+        self.topRightLayout.addWidget(self.choose_PositionsTop_Label, 4, 0)
 
 
         try:
@@ -268,6 +277,18 @@ the very bottom right"""))
 
     def define_bottom_rebars_positions(self):
 
+        beam_image = QPixmap("img/Coordinates_Bottom.jpg")
+        beam_image = beam_image.scaledToWidth(256)
+        beam_image = beam_image.scaledToHeight(256)
+        self.Cross_Section_IMG.setPixmap(beam_image)
+
+        self.choose_PositionsBottom_Label = QLabel(self.tr("""&Choose positions of rebars
+staring from the very 
+bottom right and ending with
+the very top left"""))
+        self.choose_PositionsBottom_Label.setBuddy(self.choosePositionsTopBtn)
+        self.topRightLayout.addWidget(self.choose_PositionsBottom_Label, 6, 0)
+
         try:
             for i in reversed(range(self.rebars_bottom_Layout.count())):
                 self.rebars_bottom_Layout.itemAt(i).widget().setParent(None)
@@ -280,8 +301,7 @@ the very bottom right"""))
                 self.rebars_bottom_Layout.addWidget(QLineEdit(), i, 1)
                 self.rebars_bottom_Layout.addWidget(QLabel("Y Position"), i, 2)
                 self.rebars_bottom_Layout.addWidget(QLineEdit(), i, 3)
-                print(self.rebars_top_Layout.rowCount())
-                self.topRightLayout.addLayout(self.rebars_bottom_Layout, 5+i,2)
+                self.topRightLayout.addLayout(self.rebars_bottom_Layout, 6+i,2)
 
             self.topMainLayout.setAlignment(Qt.AlignTop)
 
@@ -293,10 +313,16 @@ the very bottom right"""))
                 self.rebars_bottom_Layout.addWidget(QLineEdit(), i, 1)
                 self.rebars_bottom_Layout.addWidget(QLabel("Y Position"), i, 2)
                 self.rebars_bottom_Layout.addWidget(QLineEdit(), i, 3)
-                print(self.rebars_top_Layout.rowCount())
-                self.topRightLayout.addLayout(self.rebars_bottom_Layout, 5+i, 2)
+                self.topRightLayout.addLayout(self.rebars_bottom_Layout, 6+i, 2)
 
             self.topMainLayout.setAlignment(Qt.AlignTop)
+
+    def save_and_getInfo(self):
+        self.beamEntry = beam_insertion_intoDataBase.Insertion()
+        self.close()
+
+
+
 
 
 
